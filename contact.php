f5d2dca074516edcd512f50180cde15187482382
Phillip khoza <phillipa.khoza@itthynk.co.za>
2021-09-03 12:45:43 +0200

    changes

--- contact.php
+++ contact.php
@@ -1,24 +1,27 @@
 <?php
-    $name = $_POST['name'];
-    $visitor_email = $_POST['address'];
-    $visitor_number = $_POST['phone'];
-    $subject = $_POST['subject'];
-    $message = $_POST['Massage'];
-    
-    $email_from = 'phillipa.khoza@itthynk.co.za';
 
-    $email_subject = "$subject";
+ if(isset($_POST['submit'])){
+        $name = $_POST['name'];
+        $visitor_email = $_POST['address'];
+        $visitor_number = $_POST['phone'];
+        $subject = $_POST['subject'];
+        $message = $_POST['Massage'];
+        
+        $email_from = 'phillipa.khoza@itthynk.co.za';
 
-    $email_body = "Name: $name.\n".
-                    "Phone number: $visitor_number.\n".
-                    "Email Address: $visitor_email.\n".
-                    "Message: $message.\n";
-    
-    $to = "phillipantonk@gmail.com";
+        $email_subject = "$subject";
 
-    $headers = "From: $email_from \r\n"; 
-    $headers .= "Reply-To: $visitor_email \r\n";
-    
-    mail($to,$email_subject,$email_body,$headers);
-    header("Location: index.html");
+        $email_body = "Name: $name.\n".
+                        "Phone number: $visitor_number.\n".
+                        "Email Address: $visitor_email.\n".
+                        "Message: $message.\n";
+        
+        $to = "phillipantonk@gmail.com";
+
+        $headers = "From: $email_from \r\n"; 
+        $headers .= "Reply-To: $visitor_email \r\n";
+        
+        mail($to,$email_subject,$email_body,$headers);
+        header("Location: index.html?Mailsent");
+    }
 ?>
